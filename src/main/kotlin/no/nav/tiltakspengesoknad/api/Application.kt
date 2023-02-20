6e7137619d8e91310ab62d9a380ef715f2612567
Thor Eirik Ellefsen <thor.eirik.ellefsen@nav.no>
2023-02-20 12:19:45 +0100

    Fortsettelse av backendskjelett

--- src/main/kotlin/no/nav/tiltakspengesoknad/api/Application.kt
+++ src/main/kotlin/no/nav/tiltakspengesoknad/api/Application.kt
@@ -1,15 +1,23 @@
 package no.nav.tiltakspengesoknad.api
 
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
+import io.ktor.serialization.jackson.jackson
+import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationStarted
+import io.ktor.server.application.ApplicationStopped
+import io.ktor.server.application.install
+import io.ktor.server.auth.authenticate
+import io.ktor.server.plugins.callloging.CallLogging
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.request.httpMethod
 import io.ktor.server.routing.routing
 import mu.KotlinLogging
+import no.nav.tiltakspengesoknad.api.auth.installAuthentication
 import no.nav.tiltakspengesoknad.api.health.healthRoutes
 import no.nav.tiltakspengesoknad.api.soknad.soknadRoutes
 
-fun main() {
-    System.setProperty("logback.configurationFile", "egenLogback.xml")
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
+fun Application.module() {
     val log = KotlinLogging.logger {}
     val securelog = KotlinLogging.logger("tjenestekall")
 
@@ -17,19 +25,54 @@ fun main() {
         log.error { "Uncaught exception logget i securelog" }
         securelog.error(e) { e.message }
     }
-    log.info { "starting server" }
 
-    val server = embeddedServer(Netty, Configuration.applicationPort()) {
-        routing {
-            soknadRoutes()
-            healthRoutes(emptyList()) // TODO: Relevante helsesjekker
+    install(ContentNegotiation) {
+        jackson {
+            // TODO: Sett opp Jackson
         }
-    }.start(wait = true)
+    }
 
-    Runtime.getRuntime().addShutdownHook(
-        Thread {
-            log.info { "Stopper server" }
-            server.stop(gracePeriodMillis = 3000, timeoutMillis = 3000)
-        },
-    )
+    // Til debugging enn så lenge
+    install(CallLogging) {
+        format { call ->
+            val status = call.response.status()
+            val httpMethod = call.request.httpMethod.value
+            val userAgent = call.request.headers["User-Agent"]
+            "Status: $status, HTTP method: $httpMethod, User agent: $userAgent"
+        }
+    }
+
+  /*install(RequestValidation) {
+      TODO: Backendvalidering av søknad
+  }*/
+
+    val config = this.environment.config
+
+    installAuthentication(config)
+
+    wireRoutes()
+
+    environment.monitor.subscribe(ApplicationStarted) {
+        log.info { "Starter server" }
+    }
+    environment.monitor.subscribe(ApplicationStopped) {
+        log.info { "Stopper server" }
+    }
+}
+
+fun Application.wireRoutes() {
+  /*if (local()) {
+      //TODO: Mocking
+  }*/
+
+    // val pdlService = PdlService()
+
+    routing {
+        authenticate("userTest") {
+            // TODO pdlRoutes(pdlService)
+            soknadRoutes()
+        }
+
+        healthRoutes(emptyList())
+    }
 }
--- src/main/kotlin/no/nav/tiltakspengesoknad/api/Configuration.kt
+++ src/main/kotlin/no/nav/tiltakspengesoknad/api/Configuration.kt
@@ -6,17 +6,51 @@ import com.natpryce.konfig.EnvironmentVariables
 import com.natpryce.konfig.Key
 import com.natpryce.konfig.intType
 import com.natpryce.konfig.overriding
+import com.natpryce.konfig.stringType
 
 object Configuration {
     private val devProperties = mapOf(
-        "application.httpPort" to 8080.toString(),
+        "logback.configurationFile" to "egenLogback.xml",
+        "application.httpPort" to 8082.toString(),
     )
 
-    private val defaultProperties = ConfigurationMap(devProperties)
+    private val localProperties = ConfigurationMap(
+        mapOf(
+            "logback.configurationFile" to "logback.local.xml",
+            "TOKEN_X_WELL_KNOWN_URL"
+                to "TOKEN_X_WELL_KNOWN_URL",
+            "TOKEN_X_CLIENT_ID" to "TOKEN_X_CLIENT_ID",
+            "TOKEN_X_PRIVATE_JWK" to "TOKEN_X_PRIVATE_JWK",
+        ),
+    )
 
-    private fun config() = ConfigurationProperties.systemProperties() overriding
-        EnvironmentVariables overriding
-        defaultProperties
+    private val authProperties = mapOf(
+        "TOKEN_X_WELL_KNOWN_URL"
+            to System.getenv("TOKEN_X_WELL_KNOWN_URL"),
+        "TOKEN_X_CLIENT_ID" to System.getenv("TOKEN_X_CLIENT_ID"),
+        "TOKEN_X_PRIVATE_JWK" to System.getenv("TOKEN_X_PRIVATE_JWK"),
+    )
+
+    private val defaultProperties = ConfigurationMap(devProperties + authProperties)
+
+    private fun config() = when (System.getenv("NAIS_CLUSTER_NAME") ?: System.getProperty("NAIS_CLUSTER_NAME")) {
+        "dev-gcp" ->
+            ConfigurationProperties.systemProperties() overriding EnvironmentVariables overriding defaultProperties
+
+        "prod-gcp" ->
+            ConfigurationProperties.systemProperties() overriding EnvironmentVariables overriding defaultProperties
+
+        else -> {
+            ConfigurationProperties.systemProperties() overriding EnvironmentVariables overriding localProperties overriding defaultProperties
+        }
+    }
 
     fun applicationPort(): Int = config()[Key("application.httpPort", intType)]
+
+    data class TokenXConfig(
+        val wellKnownUrl: String = config()[Key("TOKEN_X_WELL_KNOWN_URL", stringType)],
+        val clientId: String = config()[Key("TOKEN_X_CLIENT_ID", stringType)],
+        val privateJwk: String = config()[Key("TOKEN_X_PRIVATE_JWK", stringType)],
+        val leeway: Long = 1000,
+    )
 }
