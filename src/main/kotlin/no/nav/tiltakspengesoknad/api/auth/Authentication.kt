b465885a4ed1ccdb13c70ff45bde1dfff7a23469
Erlend Vige <erlend.edvardsen.vige@nav.no>
2023-02-20 13:52:44 +0100

    Set acr=Level4 claim on token validation config

--- src/main/kotlin/no/nav/tiltakspengesoknad/api/auth/Authentication.kt
+++ src/main/kotlin/no/nav/tiltakspengesoknad/api/auth/Authentication.kt
@@ -4,48 +4,16 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.auth.Authentication
 import io.ktor.server.config.ApplicationConfig
-import mu.KotlinLogging
 import no.nav.security.token.support.v2.RequiredClaims
 import no.nav.security.token.support.v2.tokenValidationSupport
 
-private val LOG = KotlinLogging.logger {}
-private val SECURELOG = KotlinLogging.logger("tjenestekall")
 fun Application.installAuthentication(config: ApplicationConfig) {
     val acceptedIssuer = config.property("security.issuer_name").getString()
 
     install(Authentication) {
-        // Default validation
-        tokenValidationSupport(config = config)
-
-        // Only allow token with specific claim and claim value
         tokenValidationSupport(
-            name = "userTest",
             config = config,
-            requiredClaims = RequiredClaims(
-                issuer = acceptedIssuer,
-                claimMap = arrayOf("id=123test"),
-            ),
+            requiredClaims = RequiredClaims(issuer = acceptedIssuer, claimMap = arrayOf("acr=Level4")),
         )
-
-        // Only allow token that contains at least one matching claim and claim value
-        tokenValidationSupport(
-            name = "usersTest",
-            config = config,
-            requiredClaims = RequiredClaims(
-                issuer = acceptedIssuer,
-                claimMap = arrayOf("id=123test", "id=234test"),
-                combineWithOr = true,
-            ),
-        )
-
-        tokenValidationSupport(name = "ValidScope", config = config, additionalValidation = { ctx ->
-            val scopes = ctx.getClaims(acceptedIssuer)
-                ?.getStringClaim("scope")
-                ?.split(" ")
-                ?: emptyList()
-
-            val allowedScopes = setOf("nav:domain:read", "nav:domain:write")
-            scopes.any(allowedScopes::contains)
-        })
     }
 }
