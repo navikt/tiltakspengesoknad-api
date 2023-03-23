7aa5830eae6a7e47be95bee3db701d3fffa6756b
Erlend Vige <erlend.edvardsen.vige@nav.no>
2023-03-23 13:09:24 +0100

    Fjernet unødvendig init-blokk i PdlService

--- src/main/kotlin/no/nav/tiltakspenger/soknad/api/pdl/PdlService.kt
+++ src/main/kotlin/no/nav/tiltakspenger/soknad/api/pdl/PdlService.kt
@@ -4,17 +4,9 @@ import io.ktor.server.config.ApplicationConfig
 
 class PdlService(
     applicationConfig: ApplicationConfig,
-    pdlClientTokenX: PdlClientTokenX = PdlClientTokenX(config = applicationConfig),
-    pdlClientCredentials: PdlCredentialsClient = PdlCredentialsClient(config = applicationConfig),
+    private val pdlClientTokenX: PdlClientTokenX = PdlClientTokenX(config = applicationConfig),
+    private val pdlClientCredentials: PdlCredentialsClient = PdlCredentialsClient(config = applicationConfig),
 ) {
-    private var pdlClientTokenX: PdlClientTokenX
-    private var pdlClientCredentials: PdlCredentialsClient
-
-    init {
-        this.pdlClientTokenX = pdlClientTokenX
-        this.pdlClientCredentials = pdlClientCredentials
-    }
-
     suspend fun hentPersonaliaMedBarn(fødselsnummer: String, subjectToken: String): PersonDTO {
         val result = pdlClientTokenX.fetchSøker(fødselsnummer = fødselsnummer, subjectToken = subjectToken)
         if (result.isSuccess) {
