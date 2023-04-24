3a9244328b828416983a4a2574de649a674bfaba
KarlEvaldNav <karl.evald.wigestrand@nav.no>
2023-04-24 10:49:00 +0200

    Logger enda mer for å finne ut hva som gjør at jsonen fra PDL ikke kan parses
    Co-authored-by: Henrietta Eide Bleness <henrietta.eide.bleness@nav.no>

--- src/main/kotlin/no/nav/tiltakspenger/soknad/api/pdl/PdlService.kt
+++ src/main/kotlin/no/nav/tiltakspenger/soknad/api/pdl/PdlService.kt
@@ -11,21 +11,22 @@ class PdlService(
     private val pdlClientCredentials: PdlCredentialsClient = PdlCredentialsClient(config = applicationConfig),
 ) {
     suspend fun hentPersonaliaMedBarn(fødselsnummer: String, subjectToken: String): PersonDTO {
+        LOG.info { "Nå skal vi hente data fra pdl." }
         val result = pdlClientTokenX.fetchSøker(fødselsnummer = fødselsnummer, subjectToken = subjectToken)
         if (result.isSuccess) {
+            LOG.info { "Fjern denne loggingen $result" }
+            LOG.info { "Gjør om til person.." }
             val person = result.getOrNull()!!.toPerson()
+            LOG.info { "Ferdig!" }
+            LOG.info { "henter ut barnas identer.." }
             val barnsIdenter = person.barnsIdenter()
+            LOG.info { "Ferdig!" }
+            LOG.info { "mapper ut barna.." }
             val barn = barnsIdenter.map { barnsIdent ->
                 pdlClientCredentials.fetchBarn(barnsIdent).getOrNull()?.toPerson()
             }.mapNotNull { it }
+            LOG.info { "Sånn, ferdig! Det gikk jo aldeles strålende :)" }
             return person.toPersonDTO(barn)
-        }
-
-        LOG.info {
-            """
-            |Det gikk galt.. Her er result: $result
-            |Ble alt på samme linje nå?
-            """.trimMargin()
         }
 
         throw IllegalStateException("Noe gikk galt under kall til PDL")
