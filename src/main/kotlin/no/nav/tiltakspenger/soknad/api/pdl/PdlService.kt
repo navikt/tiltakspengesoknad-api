c356dd21031ca59e0954d47e3b824e66556ee06f
KarlEvaldNav <karl.evald.wigestrand@nav.no>
2023-04-25 08:53:56 +0200

    La tilbake loggingen jeg fjernet i forrige commit siden det ikke ble logget noen ting når jeg fjernet det

--- src/main/kotlin/no/nav/tiltakspenger/soknad/api/pdl/PdlService.kt
+++ src/main/kotlin/no/nav/tiltakspenger/soknad/api/pdl/PdlService.kt
@@ -1,6 +1,9 @@
 package no.nav.tiltakspenger.soknad.api.pdl
 
 import io.ktor.server.config.ApplicationConfig
+import mu.KotlinLogging
+
+val LOG = KotlinLogging.logger { }
 
 class PdlService(
     applicationConfig: ApplicationConfig,
@@ -8,13 +11,21 @@ class PdlService(
     private val pdlClientCredentials: PdlCredentialsClient = PdlCredentialsClient(config = applicationConfig),
 ) {
     suspend fun hentPersonaliaMedBarn(fødselsnummer: String, subjectToken: String): PersonDTO {
+        LOG.error { "Nå skal vi hente data fra pdl." }
         val result = pdlClientTokenX.fetchSøker(fødselsnummer = fødselsnummer, subjectToken = subjectToken)
         if (result.isSuccess) {
+            LOG.error { "Fjern denne loggingen $result" }
+            LOG.error { "Gjør om til person.." }
             val person = result.getOrNull()!!.toPerson()
+            LOG.error { "Ferdig!" }
+            LOG.error { "henter ut barnas identer.." }
             val barnsIdenter = person.barnsIdenter()
+            LOG.error { "Ferdig!" }
+            LOG.error { "mapper ut barna.." }
             val barn = barnsIdenter.map { barnsIdent ->
                 pdlClientCredentials.fetchBarn(barnsIdent).getOrNull()?.toPerson()
             }.mapNotNull { it }
+            LOG.error { "Sånn, ferdig! Det gikk jo aldeles strålende :)" }
             return person.toPersonDTO(barn)
         }
 
