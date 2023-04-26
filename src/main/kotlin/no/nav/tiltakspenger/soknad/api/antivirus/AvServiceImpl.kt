2fe38ff8e806fd2b1b4c90e2bafd79dd612b45de
Thor Eirik Ellefsen <thor.eirik.ellefsen@nav.no>
2023-04-26 13:44:58 +0200

    Fjernet unødvendig metode i AvServiceImpl

--- src/main/kotlin/no/nav/tiltakspenger/soknad/api/antivirus/AvServiceImpl.kt
+++ src/main/kotlin/no/nav/tiltakspenger/soknad/api/antivirus/AvServiceImpl.kt
@@ -8,14 +8,9 @@ class AvServiceImpl(
 ) : AvService {
 
     private val log = KotlinLogging.logger { }
-    private suspend fun scanVedlegg(vedleggsListe: List<Vedlegg>): List<AvSjekkResultat> {
-        val resultat = av.scan(vedleggsListe)
-        log.info { resultat }
-        return resultat
-    }
 
     override suspend fun gjørVirussjekkAvVedlegg(vedleggsListe: List<Vedlegg>) {
-        val resultat = scanVedlegg(vedleggsListe)
+        val resultat = av.scan(vedleggsListe).also { log.info { it } }
         val virusErFunnet = resultat.any { it.resultat == Status.FOUND }
         if (virusErFunnet) {
             throw MalwareFoundException("Skadevare funnet i vedlegg")
