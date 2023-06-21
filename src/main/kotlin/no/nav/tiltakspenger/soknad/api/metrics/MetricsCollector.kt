bd8d99b3617ef1690b4068b54070dfe8261af143
Erlend Vige <erlend.edvardsen.vige@nav.no>
2023-06-21 12:41:54 +0200

    Lagt til prefiks på metric-navn

--- src/main/kotlin/no/nav/tiltakspenger/soknad/api/metrics/MetricsCollector.kt
+++ src/main/kotlin/no/nav/tiltakspenger/soknad/api/metrics/MetricsCollector.kt
@@ -8,31 +8,31 @@ class MetricsCollector {
     private val NAMESPACE = "tpts"
 
     val ANTALL_SØKNADER_MOTTATT_COUNTER = Counter.build()
-        .name("antall_soknader_mottatt")
+        .name("tiltakspenger_soknad_antall_soknader_mottatt")
         .namespace(NAMESPACE)
         .help("Antall søknader mottatt")
         .register()
 
     val ANTALL_UGYLDIGE_SØKNADER_COUNTER = Counter.build()
-        .name("antall_ugyldige_soknader")
+        .name("tiltakspenger_soknad_antall_ugyldige_soknader")
         .namespace(NAMESPACE)
         .help("Antall ugyldige søknader forsøkt sendt inn")
         .register()
 
     val ANTALL_FEILEDE_INNSENDINGER_COUNTER = Counter.build()
-        .name("antall_soknader_feilet")
+        .name("tiltakspenger_soknad_antall_soknader_feilet")
         .namespace(NAMESPACE)
         .help("Antall feilede søknadsinnsendinger")
         .register()
 
     val ANTALL_SØKNADER_SOM_PROSESSERES = Gauge.build()
-        .name("antall_soknader_som_prosesseres")
+        .name("tiltakspenger_soknad_antall_soknader_som_prosesseres")
         .namespace(NAMESPACE)
         .help("Antall søknader som prosesseres akkurat nå")
         .register()
 
     val SØKNADSMOTTAK_LATENCY_SECONDS = Summary.build()
-        .name("soknadsmottak_latency_seconds")
+        .name("tiltakspenger_soknad_soknadsmottak_latency_seconds")
         .namespace(NAMESPACE)
         .help("Hvor lang tid det tar å prosessere en søknad (i sekunder)")
         .register()
