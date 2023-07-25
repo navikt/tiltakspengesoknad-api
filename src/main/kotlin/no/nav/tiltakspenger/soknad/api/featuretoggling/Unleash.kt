bf27acde4d84ee2dbc93fb5f542c1d312c0b1c9f
Erlend Vige <erlend.edvardsen.vige@nav.no>
2023-07-25 13:14:39 +0200

    Prøver å sette environment i unleash-oppsett

--- src/main/kotlin/no/nav/tiltakspenger/soknad/api/featuretoggling/Unleash.kt
+++ src/main/kotlin/no/nav/tiltakspenger/soknad/api/featuretoggling/Unleash.kt
@@ -7,11 +7,13 @@ import io.ktor.server.application.ApplicationEnvironment
 fun setupUnleash(environment: ApplicationEnvironment): DefaultUnleash {
     val appName = "tiltakspenger-soknad-api"
     val unleashApiUrl = "${environment.config.property("unleash.unleash_server_api_url").getString()}/api"
+    val unleashApiKey = environment.config.property("unleash.unleash_server_api_token").getString()
     val config = UnleashConfig.builder()
         .appName(appName)
+        .environment("development")
         .instanceId(appName)
         .unleashAPI(unleashApiUrl)
-        .apiKey(environment.config.property("unleash.unleash_server_api_token").getString())
+        .apiKey(unleashApiKey)
         .build()
     return DefaultUnleash(config)
 }
