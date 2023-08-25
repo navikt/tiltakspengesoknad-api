f9ad1abf996d60d6f53bf91acc95511e8b483b70
Thor Eirik Ellefsen <thor.eirik.ellefsen@nav.no>
2023-08-25 09:25:22 +0200

    Ubrukte klasser

--- src/main/kotlin/no/nav/tiltakspenger/soknad/api/joark/TokenService.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.tiltakspenger.soknad.api.joark
-
-import io.ktor.server.config.ApplicationConfig
-
-interface TokenService {
-    suspend fun getToken(config: ApplicationConfig): String
-}
