a18f8f02c0e0e09d195e871eda5835d48a2efc05
Hågen Hasle <haagenhasle@gmail.com>
2023-11-29 19:19:10 +0100

    R Fjernet bruken av jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,9 @@ plugins {
 repositories {
     mavenCentral()
     maven("https://packages.confluent.io/maven/")
-    maven("https://jitpack.io")
+    maven {
+        url = uri("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+    }
 }
 
 dependencies {
